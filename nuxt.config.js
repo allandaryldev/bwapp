86faf38a60241eaada2b3d84aaac8b4e7ad757ea
allandaryl_dev <87156467+allandaryldev@users.noreply.github.com>
2021-12-03 14:06:02 +0800

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -125,6 +125,14 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: [ "vee-validate/dist/rules" ],
+    loaders: {
+      sass: {
+        implementation: require('sass'),
+      },
+      scss: {
+        implementation: require('sass'),
+      },
+    },
   },
   hooks: {
     generate: {
